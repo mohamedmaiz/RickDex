0e11a5bd5785c4eb9d0c2ceb09b7a350114c27b9
Mohamed Maiz <mohamedmaiz9710@gmail.com>
2025-07-28 19:00:56 +0100

    feat(style): define centralized app colors

---
+++ RickDex/Core/Style/AppColors.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+enum AppColor {
+    static let background = Color(.background)
+    static let borderDefault = Color(.borderDefault)
+    static let cardBackground = Color(.cardBackground)
+    static let statusAlive = Color(.statusAlive)
+    static let statusDead = Color(.statusDead)
+    static let hintText = Color(.hintText)
+}
