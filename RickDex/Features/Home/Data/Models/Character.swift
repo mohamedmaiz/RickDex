ade8f0a6da9d83576a540e9b75c9d3ac062d4fe8
Mohamed Maiz <mohamedmaiz9710@gmail.com>
2025-07-28 19:49:36 +0100

    feat(home): add models and response structures

---
+++ RickDex/Features/Home/Data/Models/Character.swift
@@ -0,0 +1,26 @@
+struct Character: Decodable , Identifiable{
+    let id: Int
+    let name : String
+    let status : String
+    let species: String
+    let type: String
+    let gender: String
+    let origin: Origin
+    let location: Location
+    let image: String
+    let episode: [String]
+    let url: String
+    let created: String
+}
+
+
+struct Origin: Decodable {
+    let name : String
+    let url: String
+}
+
+struct Location: Decodable {
+    let name : String
+    let url: String
+}
+
---
+++ RickDex/Features/Home/Data/Responses/CharacterResponse.swift
@@ -0,0 +1,5 @@
+struct CharacterResponse : Decodable {
+    let results: [Character]
+    let info: PageInfo
+    
+}
