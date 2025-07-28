5966d6d2f2b0f29a0e815d619180228945b482d9
Mohamed Maiz <mohamedmaiz9710@gmail.com>
2025-07-28 18:49:06 +0100

    chore(app): initialize entry point and asset catalog

---
+++ RickDex/App/RickDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickDexApp.swift
+//  RickDex
+//
+//  Created by mac on 25/7/2025.
+//
+
+import SwiftUI
+
+@main
+struct RickDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
---
+++ RickDexTests/RickDexTests.swift
@@ -0,0 +1,17 @@
+//
+//  RickDexTests.swift
+//  RickDexTests
+//
+//  Created by mac on 25/7/2025.
+//
+
+import Testing
+@testable import RickDex
+
+struct RickDexTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
