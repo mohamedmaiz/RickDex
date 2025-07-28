3361856fb55013713dfb15efa1f9ce0f51f4a98b
Mohamed Maiz <mohamedmaiz9710@gmail.com>
2025-07-28 19:02:11 +0100

    feat(style): define reusable corner radius constants

---
+++ RickDex/Core/Style/AppCornerRadius.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+enum AppCornerRadius {
+    static let small: CGFloat = 4
+    static let medium: CGFloat = 12
+    static let large: CGFloat = 16
+    static let extraLarge: CGFloat = 24
+}
